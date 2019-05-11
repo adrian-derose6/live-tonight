4e6fa1d7372976858594989b9317e46cc3240d67
Adrian DeRose <adrian.derose6@gmail.com>
2019-05-10 20:16:06 -0500

    Detail Bubble shows details about event

--- src/components/Map/ShowDetailBubble.js
+++ src/components/Map/ShowDetailBubble.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Popover from '@material-ui/core/Popover';
+import CloseIcon from '@material-ui/icons/Close';
+import { Z_BLOCK } from 'zlib';
 
 const DetailBubble = ({ classes, open, anchorEl, onClose, id }) => {
     if (!open) {
@@ -9,7 +11,21 @@ const DetailBubble = ({ classes, open, anchorEl, onClose, id }) => {
     else {
         return (
             <div className={classes.container}>
-                <span onClick={onClose}>exit</span>
+                <div className={classes.eventCard}>
+                    <div style={{ flex: 1 }}>
+                        <div className={classes.imageContainer}>
+                            <img className={classes.eventImage} src="https://dk2600.files.wordpress.com/2016/12/mass-appeal-nas-it-aint-hard-to-tell.jpg?w=1000" />
+                        </div>
+                    </div>
+                    <div className={classes.eventInfo}>
+                        <h2 style={{ fontFamily: "Sharp Sans No1 Bold", color: '#00234B', marginBottom: 2 }}>Nas</h2>
+                        <p style={{ fontFamily: "Sharp Sans No1 Semibold", color: 'rgb(135, 135, 145)', fontSize: '12px', marginBottom: 3, marginTop: 1 }}>House of Blues</p>
+                        <p style={{ fontFamily: "Sharp Sans No1 Semibold", color: '#00234B', fontSize: '12px', marginTop: 2}}>329 N Dearborn St,<br/>Chicago, IL 60654</p>
+                    </div>
+                </div>
+                <div className={classes.exitContainer}>
+                    <CloseIcon className={classes.exitIcon} onClick={onClose}/>
+                </div>
             </div>
         )
     }
@@ -17,13 +33,69 @@ const DetailBubble = ({ classes, open, anchorEl, onClose, id }) => {
 
 const styles = theme => ({
     container: {
+        display: 'flex',
+        flexDirection: 'row',
         position: 'absolute',
-        width: '186px',
-        height: '140px',
+        bottom: 1.6,
+        width: '200px',
+        height: '120px',
         backgroundColor: 'white',
-        border: '1px solid gray',
+        border: '1px solid rgb(0, 35, 75, 0.4)',
         borderRadius: '3px',
-        boxShadow: '1px 0px 2px -1px rgba(135,119,135,1)'
+        borderBottomLeftRadius: '0px',
+        boxShadow: '1px 0px 2px -1px rgba(135,119,135,1)',
+        paddingLeft: 10,
+        paddingTop: 5,
+        paddingBottom: 5,
+    },
+    eventCard: {
+        height: '100%',
+        width: 'max-content',
+        flex: 4,
+        display: 'flex',
+        overflowX: 'scroll',
+        overflowY: 'hidden',
+        whiteSpace: 'nowrap',
+    },
+    exitContainer: {
+        flex: 1,
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    exitIcon: {
+        color: 'darkgray',
+        fontSize: 20,
+        '&:hover': {
+            cursor: 'pointer'
+        }
+    },
+    imageContainer: {
+        borderRadius: '50%',
+        width: 60,
+        height: 60,
+        marginTop: 15,
+        position: 'relative',
+        overflow: 'hidden'
+    },
+    eventInfo: {
+        flexDirection: 'column',
+        height: '100%',
+        marginLeft: 10,
+        flex: 4,
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start'
+    },
+    eventImage: {
+        position: 'absolute',
+        top: '-100%',
+        left: '-100%',
+        bottom: '-100%',
+        right: '-100%',
+        objectFit: 'cover',
+        width: 60,
+        height: 60,
+        margin: 'auto'
+        
     }
 });
 
